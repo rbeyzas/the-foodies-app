fe1fec5121ef891840f4869c25609e6c995c35fe
rbeyzas <rukiyebeyzasarikaya@hotmail.com>
2023-12-07 23:00:52 +0300

    image slideshow and use of use client

--- components/MainHeader/MainHeader.js
+++ components/MainHeader/MainHeader.js
@@ -1,30 +1,35 @@
 import Link from 'next/link';
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
+import Image from 'next/image';
+import MainHeaderBackground from './main-header-background';
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
-        Next Level Food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          Next Level Food
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/meals/share">Share Meal</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/meals/share">Share Meal</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
